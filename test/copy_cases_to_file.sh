3f4bf4b70cbf8c3cbbf71d525088b9258048318c
ed <haslamenator@gmail.com>
2019-12-12 11:00:29 +1300

    Added court name field to test files

--- test/copy_cases_to_file.sh
+++ test/copy_cases_to_file.sh
@@ -10,12 +10,18 @@ WITH case_json_cte (case_id,block_id,case_json) AS (
 		SELECT
 			ROUND(a.id,-2) AS block_id,
 			a.id AS case_id,
-			a.CASE_DATE,
+			a.CASE_DATE as "date",
+			a.citation,
 			a.CASE_NAME,
 			a.CASE_TEXT,
-			a.legislation
+			a.legislation,
+			a.Court_Name
 		FROM (
-			SELECT ca.*, (SELECT JSONB_AGG(SUB) FROM
+			SELECT 
+				ca.*,
+				cit.CITATION,
+				co.COURT_NAME,
+				(SELECT JSONB_AGG(SUB) FROM
 						(
 							SELECT 
 								l.title,
@@ -26,6 +32,12 @@ WITH case_json_cte (case_id,block_id,case_json) AS (
 							WHERE l2c.case_id=cA.id
 						) AS SUB) AS legislation
 			FROM cases.cases.CASES ca
+			INNER JOIN cases.cases.CASE_CITATIONS cit
+				ON ca.id=cit.case_id
+			LEFT JOIN cases.cases.COURT_TO_CASES c2ca
+				ON c2ca.CASE_ID=ca.ID
+			LEFT JOIN cases.cases.COURTS co
+				ON co.ID=c2ca.id
 			ORDER BY ca.id
 		) a
 	) ROWS
@@ -34,4 +46,4 @@ SELECT JSONB_AGG(ct1.case_json)
 FROM case_json_cte ct1
 GROUP BY ct1.block_id
 ORDER BY ct1.block_id;
-' | sudo -u postgres psql -d cases | sed '1,2d' > full_documents.json
+' | sudo -u postgres psql -d cases | sed '1,2d' > documents.json
